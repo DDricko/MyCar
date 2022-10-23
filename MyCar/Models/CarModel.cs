a060824757bca8b5f826186c31c029f8062153fd
Rodrigo Waltmann Nascimento <rodrigowaltmann.n@gmail.com>
2022-10-23 18:19:13 -0300

    Criação da CarModel

---
+++ MyCar/Models/CarModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MyCar.Models
+{
+    public class CarModel
+    {
+        public int id { get; set; }
+        public string Plate { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+    }
+}
